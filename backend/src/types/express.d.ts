f0b624b5dbe05c083bc7c6df7175bca7d290d918
Mao Hashimoto <hashi.mao.050925@gmail.com>
2025-11-25 11:12:19 +0900

    lineログイン認証

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,16 +1,6 @@
 import "express-session";
 import type { JwtPayload } from '../utils/jwt.js';
 
-declare module "express-session" {
-  interface SessionData {
-    lineState?: {
-      value: string;
-      nonce: string;
-      createdAt: number;
-    };
-  }
-}
-
 declare global {
   namespace Express {
     interface Request {
