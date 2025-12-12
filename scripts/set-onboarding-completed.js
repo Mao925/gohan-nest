7c99b8d73f2dac77cbeaff67cf1c508110342568
Mao Hashimoto <hashi.mao.050925@gmail.com>
2025-12-12 11:55:19 +0900

    定員を変更できるようにする

---
+++ scripts/set-onboarding-completed.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.user.updateMany({
+    data: { hasCompletedOnboarding: true },
+    where: { hasCompletedOnboarding: false },
+  });
+
+  console.log(`Updated ${result.count} users`);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
